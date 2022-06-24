dfd3f2189d683a8b89f3aa117298de721b6bdfc0
tymason <tymason@DESKTOP-OQEA4A3>
2022-06-23 23:42:38 -0600

    Started slash comands with bot

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 require('dotenv').config()
 const Discord = require('discord.js')
 const axios = require('axios')
+const chalk = require('chalk')
+const fs = require('fs')
 
 const client = new Discord.Client({
     intents : ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS']
 })
 
-client.login()
+client.on("ready", async() => {
+    console.log(chalk.bold.green(`Discord Bot ${client.user.tag} is online!`))
+    const commands = await client.application.commands.fetch()
+    const mapslash = require('./slash-json/map.json')
+    if (!commands.get("map")) {
+        console.log(chalk.red("Map command isn't registered"))
+        client.application.commands.create(mapslash)
+        .catch(e => {
+            console.error(e)
+        })
+        .then(slash => {
+            console.log(`Slash command ${slash.name} is now being deployed.`)
+        })
+    }
+})
+
+
+client.login(process.env.TOKEN)
